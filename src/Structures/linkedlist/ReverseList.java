ab8f9365372fdbc53c65b1f86c81bceaa657a009
gaozx <gaozx@mail.ustc.edu.cn>
2019-09-21 21:48:36 +0800

    反转链表

--- src/Structures/linkedlist/ReverseList.java
+++ src/Structures/linkedlist/ReverseList.java
@@ -15,3 +15,23 @@ public class ReverseList {
         return t;
     }
 }
+/*
+public ListNode ReverseList(ListNode head) {
+        if(head == null || head.next == null)return head;
+//        ListNode temp = ReverseList(head.next);
+//        head.next.next = head;
+//        head.next = null;
+//        return temp;
+        ListNode cur = head;
+        ListNode pre = null;
+        ListNode next = null;
+        while(cur != null){
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+*
+* */
