c923e4046547f001a2fa75d6b600cce6b1b1da37
gaozx95 <602753653@qq.com>
2019-09-24 22:31:52 +0800

    LCS待完善，还需要打印出来

--- src/sort/Quick.java
+++ src/sort/Quick.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Quick {
     public static void main(String[] args) {
         int[] arr = new int[]{3,2,5,7,1,6,4};
-        quick(arr,0,arr.length-1);
+        quickSort(arr,0,arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
     static public void quick(int[] arr,int start,int end){
@@ -27,6 +27,27 @@ public class Quick {
 //        System.out.println(Arrays.toString(arr));
         return l;
 
+    }
+    static public void quickSort(int[] arr,int l,int r){
+        int i =l;
+        int j = r;
+        int p = arr[(l+r)/2];
+        while (i<=j){
+            while (arr[j]>p) j--;
+            while (arr[i]<p) i++;
+
+            if(i<=j){
+                swapArr(arr,i,j);
+                i++;
+                j--;
+            }
+        }
+        if(l<j){
+            quickSort(arr,l,j);
+        }
+        if(i<r){
+            quickSort(arr,i,r);
+        }
     }
     static public void swapArr(int[] arr,int i,int j){
         int tmp = arr[i];
